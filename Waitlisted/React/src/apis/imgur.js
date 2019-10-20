277eac3c60a4d79395b802900b6a9d95fd4b37db
ANUBHAVNATANI <anubhavnatani@gmail.com>
2019-10-20 13:42:05 +0530

    React code addition

--- Waitlisted/React/src/apis/imgur.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-	baseURL: 'https://api.imgur.com/3',
-	headers: {
-		Authorisation: `Bearer ebaffdcbe8ea8bc926654f794cb76ad74dc5c490`
-	}
-});
--- Waitlisted/React/src/reducers/setComplaintsReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-	switch (action.type) {
-		case 'SET_COMPLAINTS':
-			return action.payload;
-		default:
-			return state;
-	}
-};
